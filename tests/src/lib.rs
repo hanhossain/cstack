0eed15b75d07317c6212e27ff3ebfd225c74a57e
Han Hossain <ahanuf12@gmail.com>
2022-07-25 19:29:22 -0700

    part 8

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -136,4 +136,50 @@ mod tests {
         let expected = vec!["db > (1, user1, person1@example.com)", "Executed.", "db > "];
         assert_eq!(output, expected);
     }
+
+    #[test]
+    fn prints_constants() {
+        let db = Database::new();
+        let output = db.run_script(vec![".constants", ".exit"]);
+        assert_eq!(
+            output,
+            vec![
+                "db > Constants:",
+                "ROW_SIZE: 293",
+                "COMMON_NODE_HEADER_SIZE: 6",
+                "LEAF_NODE_HEADER_SIZE: 10",
+                "LEAF_NODE_CELL_SIZE: 297",
+                "LEAF_NODE_SPACE_FOR_CELLS: 4086",
+                "LEAF_NODE_MAX_CELLS: 13",
+                "db > ",
+            ]
+        );
+    }
+
+    #[test]
+    fn prints_structure_of_one_node_btree() {
+        let db = Database::new();
+        let input = vec![
+            "insert 3 user3 person3@example.com",
+            "insert 1 user1 person1@example.com",
+            "insert 2 user2 person2@example.com",
+            ".btree",
+            ".exit",
+        ];
+        let output = db.run_script(input);
+        assert_eq!(
+            output,
+            vec![
+                "db > Executed.",
+                "db > Executed.",
+                "db > Executed.",
+                "db > Tree:",
+                "leaf (size 3)",
+                "  - 0 : 3",
+                "  - 1 : 1",
+                "  - 2 : 2",
+                "db > ",
+            ]
+        );
+    }
 }
