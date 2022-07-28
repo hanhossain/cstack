e94adfc0b8ef1f0a779906c4c2d8c6500fe13839
Han Hossain <ahanuf12@gmail.com>
2022-07-27 17:56:39 -0700

    ignore callbacks from .h

--- cstacksys/src/lib.rs
+++ cstacksys/src/lib.rs
@@ -6,6 +6,8 @@ pub extern "C" fn hello_world() {
     println!("Hello world!");
 }
 
+#[allow(unused_doc_comments)]
+/// cbindgen:ignore
 extern "C" {
     fn callback_printf(string: *const libc::c_char);
 }
