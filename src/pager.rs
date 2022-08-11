be538eff51aba515519aa04dfcb8b15bf2dd4d09
Han Hossain <ahanuf12@gmail.com>
2022-08-11 15:52:01 -0700

    move to rust based file usage

--- src/pager.rs
+++ src/pager.rs
@@ -1,9 +1,7 @@
 use crate::node::{CommonNode, InternalNode, LeafNode, Node};
-use libc::{
-    c_uint, c_void, close, lseek, open, read, write, EXIT_FAILURE, O_CREAT, O_RDWR, SEEK_END,
-    SEEK_SET, S_IRUSR, S_IWUSR,
-};
-use std::ffi::CString;
+use libc::EXIT_FAILURE;
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Seek, SeekFrom, Write};
 use std::process::exit;
 use std::ptr::null_mut;
 
@@ -11,35 +9,30 @@ pub const TABLE_MAX_PAGES: usize = 100;
 pub const PAGE_SIZE: usize = 4096;
 
 pub struct Pager {
-    file_descriptor: i32,
+    file: File,
     file_length: u32,
     pub num_pages: u32,
     pages: [*mut u8; TABLE_MAX_PAGES],
 }
 
 impl Pager {
-    pub unsafe fn open(filename: &str) -> Pager {
-        let filename_owned = CString::new(filename).unwrap();
-        let filename = filename_owned.as_ptr();
-        let fd = open(
-            filename,
-            O_RDWR | O_CREAT,
-            S_IRUSR as c_uint | S_IWUSR as c_uint,
-        );
-        if fd == -1 {
-            println!("Unable to open file");
-            exit(EXIT_FAILURE);
-        }
+    pub fn open(filename: &str) -> Pager {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(filename)
+            .unwrap();
 
-        let file_length = lseek(fd, 0, SEEK_END);
+        let file_length = file.seek(SeekFrom::End(0)).unwrap();
         if file_length as usize % PAGE_SIZE != 0 {
             println!("Db file is not a whole number of pages. Corrupt file.");
             exit(EXIT_FAILURE);
         }
 
         Pager {
+            file,
             file_length: file_length as u32,
-            file_descriptor: fd,
             num_pages: file_length as u32 / PAGE_SIZE as u32,
             pages: [null_mut(); TABLE_MAX_PAGES],
         }
@@ -57,12 +50,6 @@ impl Pager {
             pager.pages[i] = null_mut();
         }
 
-        let result = close(pager.file_descriptor);
-        if result == -1 {
-            println!("Error closing db file.");
-            exit(EXIT_FAILURE);
-        }
-
         for i in 0..TABLE_MAX_PAGES {
             let page = pager.pages[i];
             if !page.is_null() {
@@ -73,7 +60,7 @@ impl Pager {
     }
 
     pub fn page(&mut self, page_num: usize) -> Node {
-        unsafe { Node::from(self.get_page(page_num)) }
+        Node::from(self.get_page(page_num))
     }
 
     pub unsafe fn new_leaf_page(&mut self, page_num: usize) -> LeafNode {
@@ -90,7 +77,7 @@ impl Pager {
         node
     }
 
-    unsafe fn get_page(&mut self, page_num: usize) -> CommonNode {
+    fn get_page(&mut self, page_num: usize) -> CommonNode {
         if page_num > TABLE_MAX_PAGES {
             println!("Tried to fetch page number out of bounds. {page_num} > {TABLE_MAX_PAGES}");
             exit(EXIT_FAILURE);
@@ -98,7 +85,7 @@ impl Pager {
 
         if self.pages[page_num].is_null() {
             // Cache miss. Allocate memory and load from file.
-            let page = Box::into_raw(Box::new([0u8; PAGE_SIZE]));
+            let mut page = Box::new([0u8; PAGE_SIZE]);
             let mut num_pages = self.file_length as usize / PAGE_SIZE;
 
             // We might save a partial page at the end of the file
@@ -107,19 +94,13 @@ impl Pager {
             }
 
             if page_num <= num_pages {
-                lseek(
-                    self.file_descriptor,
-                    page_num as i64 * PAGE_SIZE as i64,
-                    SEEK_SET,
-                );
-                let bytes_read = read(self.file_descriptor, page as *mut c_void, PAGE_SIZE);
-                if bytes_read == -1 {
-                    println!("Error reading file");
-                    exit(EXIT_FAILURE);
-                }
+                self.file
+                    .seek(SeekFrom::Start(page_num as u64 * PAGE_SIZE as u64))
+                    .unwrap();
+                self.file.read(page.as_mut_slice()).unwrap();
             }
 
-            self.pages[page_num] = page as *mut u8;
+            self.pages[page_num] = Box::into_raw(page) as *mut u8;
 
             if page_num >= self.num_pages as usize {
                 self.num_pages = page_num as u32 + 1;
@@ -135,27 +116,13 @@ impl Pager {
             exit(EXIT_FAILURE);
         }
 
-        let offset = lseek(
-            self.file_descriptor,
-            (page_num * PAGE_SIZE) as i64,
-            SEEK_SET,
-        );
+        self.file
+            .seek(SeekFrom::Start(page_num as u64 * PAGE_SIZE as u64))
+            .unwrap();
 
-        if offset == -1 {
-            println!("Error seeking");
-            exit(EXIT_FAILURE);
-        }
-
-        let bytes_written = write(
-            self.file_descriptor,
-            self.pages[page_num] as *mut c_void,
-            PAGE_SIZE,
-        );
-
-        if bytes_written == -1 {
-            println!("Error writing");
-            exit(EXIT_FAILURE);
-        }
+        self.file
+            .write_all(std::slice::from_raw_parts(self.pages[page_num], PAGE_SIZE))
+            .unwrap();
     }
 
     // TODO: Until we start recycling free pages, new pages will always
